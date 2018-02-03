499d534c0bc500ece11b861ff3d602f66e8b3707
fanyazhi <yf92@cornell.edu>
2018-02-03 16:18:54 -0500

    more hacker practice

---
+++ HackerPractice/JanWeek01_yf92/exceptionHandling.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x = 0.0; double y = 0.0;
+    double doubleResult1 = 1/x; double doubleResult2 = y/x;
+    cout<<doubleResult1<<endl;
+    cout<<doubleResult2<<endl;
+
+    long m = 0; long n = 0;
+    int intResult1 = 1/m; int intResult2 = m/n;
+    cout<<intResult1<<endl;
+    cout<<intResult2<<endl;
+    return 0;
+
+
+}
