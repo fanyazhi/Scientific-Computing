2e434131505052305f5be9c8f7a8a5b3844c33f6
fanyazhi <yf92@cornell.edu>
2018-02-10 22:42:20 -0500

    corrected relative error

--- HackerPractice/FebWeek02_yf92/relativeError.cpp
+++ HackerPractice/FebWeek02_yf92/relativeError.cpp
@@ -3,13 +3,13 @@
 #include <iomanip>
 
 using namespace std;
-double f(double x)
+double f(double x, double c)
 {
-    return pow(x,2);
+    return pow(x,2)+pow(c,10);
 }
-double fDerivative(double x, double h)
+double fDerivative(double x, double h, double c)
 {
-    float fder = (f(x+h)-f(x))/h;
+    float fder = (f(x+h,c)-f(x,c))/h;
     return fder;
 }
 
@@ -18,10 +18,14 @@ int main()
 	for (double h = 0.1; h > pow(10,-18); h = h*0.1)
     {
 		double x = 1;
-		double fder = fDerivative(x, h);
+		double c = 0;
+		double fder = fDerivative(x, h, c);
+		double c2 = 10;
+		double fder2 = fDerivative(x, h, c2);
 		
 		double error = 2-fder;
-        cout << setprecision (15) <<error<<endl;
+		double error2 = 2-fder2;
+        cout<<error<<" "<<error2<<endl;
 	}
 	return 0;
 }
