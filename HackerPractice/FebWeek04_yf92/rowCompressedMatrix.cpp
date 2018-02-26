6dcde5d558572a0e6c888a6e4ec39255755fafc4
fanyazhi <yf92@cornell.edu>
2018-02-25 23:12:03 -0500

    fixed bugs in rowCompressed

--- HackerPractice/FebWeek04_yf92/rowCompressedMatrix.cpp
+++ HackerPractice/FebWeek04_yf92/rowCompressedMatrix.cpp
@@ -1,62 +1,105 @@
 #include <iostream>
 #include <vector>
-#include <cstream>
+
 using namespace std;
 class matrix3
 {
-	int input;
+	int input, i, j;
 	vector<int> value;
 	vector<int> rowPtr;
 	vector<int> colInd; 
 	vector<int> rowLength;
+	vector<int> xVector;
+	vector<int> product;
+	double a;
 	public :
 		void values();
-		int rowPermute();
+		void rowPermute(int i, int j);
+		void rowScaling(int i, int j, double a);
+		void productAx();
 };
 void matrix3::values()
 {
+	//input row compressed matrix
 	cout << "Enter the values, enter 0 when finished   ";
 	while ((cin >> input) && (input!=0)){
 		value.push_back(input);
 	}
-	cout << "Enter the row pointer, enter 0 when finished   "; 
-	while ((cin >> input) && (input!=0)){
+	cout << "Enter the row pointer, enter 999 when finished   "; 
+	while ((input!=999) && (cin >> input)){
 		rowPtr.push_back(input);
 	}
-	cout << "Enter the column indicies\n\n";
+	cout << "Enter the column indicies, enter 0 when finished  \n\n";
 	while ((cin >> input) && (input!=0)){
 		colInd.push_back(input);
 	}
+	//input vector x if needed
+	cout << "Enter the vector x, enter 0 if not needed or is finished   "; 
+	while ((cin >> input) && (input!=0)){
+		xVector.push_back(input);
+	}
+
 }
-void matrix3::rowPermute()
+void matrix3::rowPermute(int i, int j)
 {
 	cout << "switch row[i] and row[j] for matrix A and vector x\n";
-	for (i=0;i<sizeof(rowPtr)-1;i++){
+	for (int i=0;i<sizeof(rowPtr)-1;i++){
 		rowLength.push_back(rowPtr[i+1]-rowPtr[i]);
 	}
 	
 	swap(rowPtr[i],rowPtr[j]);
 	//print out results
+	vector<int>::iterator it;
+	it = value.begin();
 	for (it=value.begin(); it<value.end(); it++)
 		std::cout << ' ' << *it;
     std::cout << '\n';
+	it = colInd.begin();
 	for (it=colInd.begin(); it<colInd.end(); it++)
 		std::cout << ' ' << *it;
     std::cout << '\n';
+	it = rowPtr.begin();
 	for (it=rowPtr.begin(); it<rowPtr.end(); it++)
 		std::cout << ' ' << *it;
     std::cout << '\n';
 
 }
-void matrix3::rowScaling()
+void matrix3::rowScaling(int i, int j, double a)
 {
 	cout << "row scaling of the matrix is\n";
+	int k,l,c;
+	vector<int> des;
+	vector<int> fac;
+	//extract destination row
+	for(k = rowPtr[i-1]; k < rowPtr[i]; k++){
+		if(k-rowPtr[i-1]+1 == colInd[i])
+			des[k-rowPtr[i-1]+1] = value[k];
+		else des[k-rowPtr[i-1]+1] = 0;
+    }
+    //extract factor row
+    for(l=rowPtr[j-1]; l<rowPtr[j]; l++){
+    	if((l-rowPtr[j-1]+1) == colInd[l])
+    		fac[l-rowPtr[j-1]+1] = value[l];
+    	else fac[l-rowPtr[j-1]+1] = 0;
+	}
+	
+	//calculate the result after *a and addition
+	c = *max_element(colInd.begin(),colInd.end());
+	for(k=1; k<=c; k++){
+		des[k] += a*fac[k];
+		value[rowPtr[i-1]] = des[k];
+	}    
 
   
 }
 void matrix3::productAx()
 {
-	cout << "product of the matrix and vector is\n";
+	cout << "switch row[i] and row[j] for matrix A and vector x\n";
+		for (i=1;i<=sizeof(rowPtr);i++){
+		for (j=rowPtr[i];j<=rowPtr[i+1]-1;j++){
+		product[i]=product[i]+value[j]*xVector[colInd[j]];
+		}
+	}
 }
 int main()
 {
@@ -84,7 +127,7 @@ int input,k,l;
 			cin >> k;
 			cin >> l;
 			cin >> q;
-			m.rowScale(k,l,q);
+			m.rowScaling(k,l,q);
 			break;
 
 		case 3:
